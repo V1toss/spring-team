800d666c240edd663ede896eb534c5182ca11918
andreiHi <andreih1981@gmail.com>
2018-08-28 23:16:17 +0300

    entity

---
+++ entities/src/main/java/com/bank/application/model/EntityType.java
@@ -0,0 +1,11 @@
+package com.bank.application.model;
+
+/**
+ * @author Hincu Andrei (andreih1981@gmail.com)on 28.08.2018.
+ * @version $Id$.
+ * @since 0.1.
+ */
+public enum EntityType {
+
+    NEWS, USER , NEWSLINE,  BROADCAST
+}
